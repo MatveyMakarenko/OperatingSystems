335968cc0bf1c5c04393795d69e81cfcea1e8865
Matvey Makarenko <Matvey242005@gmail.com>
2025-12-17 23:21:49 +0700

    first commit

---
+++ lab3/OS_lab3.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+
+static int __init tsulab_init(void)
+{
+    pr_info("Welcome to the Tomsk State University\n");
+    return 0;
+}
+
+static void __exit tsulab_exit(void)
+{
+    pr_info("Tomsk State University forever!\n");
+}
+
+module_init(tsulab_init);
+module_exit(tsulab_exit);
+
+MODULE_LICENSE("GPL");
