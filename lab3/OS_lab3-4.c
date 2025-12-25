bed350d7c27395a00259347358b2232ad38153a4
MatveyMakarenko <148697980+MatveyMakarenko@users.noreply.github.com>
2025-12-25 14:58:14 +0700

    Update OS_lab3-4.c

--- lab3/OS_lab3-4.c
+++ lab3/OS_lab3-4.c
@@ -4,70 +4,77 @@
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
+#include <linux/time.h>
+#include <linux/math64.h>
 
-#define PROCFS_NAME "tsulab"
+#define procfs_name "tsulab"
 
-static struct proc_dir_entry *our_proc_file = NULL;
+static struct proc_dir_entry* our_proc_file = NULL;
 
-// Параметры задачи
-#define EARTH_EQUATOR_KM    40075L
-#define HINDENBURG_SPEED_KMH 126L
+#define EARTH_EQUATOR_KM    40075LL
+#define HINDENBURG_SPEED_KMH 126LL
 
-static ssize_t procfile_read(struct file *file, char __user *buffer,
-                             size_t len, loff_t *offset)
+static ssize_t profile_read(struct file* file_pointer, char __user* buffer,
+    size_t buffer_length, loff_t* offset)
 {
-    // Вычисление в минутах для точности
-    const long total_minutes = (EARTH_EQUATOR_KM * 60L) / HINDENBURG_SPEED_KMH;
+    char s[256];
+    int len;
+    ssize_t bytes_to_copy;
 
-    const long days = total_minutes / (60L * 24L);
-    const long hours = (total_minutes % (60L * 24L)) / 60L;
-    const long minutes = total_minutes % 60L;
+    
+    long long total_minutes = (EARTH_EQUATOR_KM * 60LL) / HINDENBURG_SPEED_KMH;
+    long days = total_minutes / (24L * 60L);
+    long hours = (total_minutes % (24L * 60L)) / 60L;
+    long minutes = total_minutes % 60L;
 
-    char msg[128];
-    int msg_len = scnprintf(msg, sizeof(msg),
+    len = scnprintf(s, sizeof(s),
         "Hindenburg will return in %ld days %ld hours %ld minutes\n",
         days, hours, minutes);
 
-    if (*offset >= msg_len)
+    if (*offset >= len) {
         return 0;
-
-    if (len > (size_t)(msg_len - *offset))
-        len = msg_len - *offset;
-
-    if (copy_to_user(buffer, msg + *offset, len))
+    }
+    bytes_to_copy = len - *offset;
+    if (buffer_length < bytes_to_copy) {
+        bytes_to_copy = buffer_length;
+    }
+    if (copy_to_user(buffer, s + *offset, bytes_to_copy)) {
         return -EFAULT;
+    }
 
-    *offset += len;
-    return len;
+    *offset += bytes_to_copy;
+    return bytes_to_copy;
 }
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
 static const struct proc_ops proc_file_fops = {
-    .proc_read = procfile_read,
+    .proc_read = profile_read,
 };
 #else
 static const struct file_operations proc_file_fops = {
-    .read = procfile_read,
+    .read = profile_read,
 };
 #endif
 
-static int __init tsulab_init(void)
+static int __init procfs1_init(void)
 {
-    pr_info("Welcome to the Tomsk State University\n");
-    our_proc_file = proc_create(PROCFS_NAME, 0644, NULL, &proc_file_fops);
+    our_proc_file = proc_create(procfs_name, 0644, NULL, &proc_file_fops);
     if (!our_proc_file) {
-        pr_err("Failed to create /proc/%s\n", PROCFS_NAME);
+        pr_err("Failed to create /proc/%s\n", procfs_name);
         return -ENOMEM;
     }
+    pr_info("Welcome to the Tomsk State University\n");
     return 0;
 }
 
-static void __exit tsulab_exit(void)
+static void __exit procfs1_exit(void)
 {
-    proc_remove(our_proc_file);
+    if (our_proc_file) {
+        proc_remove(our_proc_file);
+    }
     pr_info("Tomsk State University forever!\n");
 }
 
-module_init(tsulab_init);
-module_exit(tsulab_exit);
+module_init(procfs1_init);
+module_exit(procfs1_exit);
 MODULE_LICENSE("GPL");
